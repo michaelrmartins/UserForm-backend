25cb666bb131067f5b0d78186d2f3f5941b76368
Michael Martins <michaelmartins.js@hotmail.com>
2024-11-03 23:37:44 -0300

    add handler to add user on database

--- models/usersHandler.js
+++ models/usersHandler.js
@@ -1,16 +1,25 @@
 const database = require("./connection");
 
 // Import Queries
-const { queryGetAllUsers } = require("./queries/userQueries")
+const { queryGetAllUsers, queryRegNewUser,} = require("./queries/userQueries")
 
+// Get Users
 const getUsers = async () => {
     const [rows] = await database.execute(queryGetAllUsers);
     return rows;
-
 };
 
+// Register new User
+const regUser = async (userData) => {
+    const { username, birthdate, cpf, id_role} = userData;
 
+    const [result] = await database.execute(queryRegNewUser,
+        [username, birthdate, cpf, id_role])
+        return result;
+}
 
+// Exports >>>> 
 module.exports = {
-    getUsers
+    getUsers, 
+    regUser,
 }
