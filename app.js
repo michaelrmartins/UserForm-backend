dfaf13d758224cbe5e24390a48cd60d571d81a1f
Michael Martins <michaelmartins.js@hotmail.com>
2024-11-03 23:35:05 -0300

    Adjust on express to handle json from requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require('express');  // Importa o Express
 const app = express();               // Faz um Alias, para ao invés de usar "express", usa o "app"
 
+// configura o Express para interpretar requisições em Json
+app.use(express.json());
+
 // Importing Routes
 const routes = require('./routes');
 app.use('/', routes);
