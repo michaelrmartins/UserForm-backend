15156c72dadcd07bc83ed81788c30ca7a9e5bcd9
Michael Martins <michaelmartins.js@hotmail.com>
2024-11-03 23:36:46 -0300

    add function  to add user

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,30 @@ const getUsers = async (req, resp ) => {
 }
 
 
+// Register Users Controller
+const regUser = async (req, resp ) => {
+        console.log("I am a Controller...")
+
+       try{ 
+        console.log("let's try execute this... ")
+        const { USERNAME, BIRTHDATE, CPF, ID_ROLE } = req.body;
+        const result = await userHandler.regUser({
+            username: USERNAME,
+            birthdate: BIRTHDATE,
+            cpf: CPF,  
+            id_role: ID_ROLE          
+        });
+        return resp.status(201).json({message: `message:User Registred Successfully`, result})
+    } catch(error) {
+        console.log("Controller - regUse error:", error);
+        return resp.status(500).json({message: "Error Registring user", error})
+    } // try finally
+
+
+}; // end regUser
+
 // Exports
 module.exports = {
-    getUsers
+    getUsers,
+    regUser,
 }
