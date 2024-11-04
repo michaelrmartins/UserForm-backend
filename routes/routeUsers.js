a84e83d4011b592ef06d584c8f290ccee88ae737
Michael Martins <michaelmartins.js@hotmail.com>
2024-11-03 23:39:20 -0300

    Minnor adjusts

--- routes/routeUsers.js
+++ routes/routeUsers.js
@@ -4,6 +4,7 @@ const queryController = require('../controllers/userController');
 
 //router.get("/", (req, resp) => resp.status(200).send("Rota Usuarios!!"));
 router.get("/", queryController.getUsers)
+router.post("/", queryController.regUser)
 
 module.exports = router;
 
